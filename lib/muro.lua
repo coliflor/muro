f7c2874859ab71ee134b51368a08db2b47723652
Coliflor <javi.forano@gmail.com>
2023-11-07 23:45:38 +0100

    libdeflate test not sucesfull

--- lib/muro.lua
+++ lib/muro.lua
@@ -1,4 +1,6 @@
 local d = require "lib.helper"
+local libdeflate = require "lib.libdeflate"
+
 local muro = {}
 
 muro.characterTable = {
@@ -108,106 +110,106 @@ muro.characterTable = {
 
 --[[
 
-RGB: (99, 99, 99)
-RGB: (98, 98, 98)
-RGB: (97, 97, 97)
-RGB: (96, 96, 96)
-RGB: (95, 95, 95)
-RGB: (94, 94, 94)
-RGB: (93, 93, 93)
-RGB: (92, 92, 92)
-RGB: (91, 91, 91)
-RGB: (90, 90, 90)
-RGB: (89, 89, 89)
-RGB: (88, 88, 88)
-RGB: (87, 87, 87)
-RGB: (86, 86, 86)
-RGB: (85, 85, 85)
-RGB: (84, 84, 84)
-RGB: (83, 83, 83)
-RGB: (82, 82, 82)
-RGB: (81, 81, 81)
-RGB: (80, 80, 80)
-RGB: (79, 79, 79)
-RGB: (78, 78, 78)
-RGB: (77, 77, 77)
-RGB: (76, 76, 76)
-RGB: (75, 75, 75)
-RGB: (74, 74, 74)
-RGB: (73, 73, 73)
-RGB: (72, 72, 72)
-RGB: (71, 71, 71)
-RGB: (70, 70, 70)
-RGB: (69, 69, 69)
-RGB: (68, 68, 68)
-RGB: (67, 67, 67)
-RGB: (66, 66, 66)
-RGB: (65, 65, 65)
-RGB: (64, 64, 64)
-RGB: (63, 63, 63)
-RGB: (62, 62, 62)
-RGB: (61, 61, 61)
-RGB: (60, 60, 60)
-RGB: (59, 59, 59)
-RGB: (58, 58, 58)
-RGB: (57, 57, 57)
-RGB: (56, 56, 56)
-RGB: (55, 55, 55)
-RGB: (54, 54, 54)
-RGB: (53, 53, 53)
-RGB: (52, 52, 52)
-RGB: (51, 51, 51)
-RGB: (50, 50, 50)
-RGB: (49, 49, 49)
-RGB: (48, 48, 48)
-RGB: (47, 47, 47)
-RGB: (46, 46, 46)
-RGB: (45, 45, 45)
-RGB: (44, 44, 44)
-RGB: (43, 43, 43)
-RGB: (42, 42, 42)
-RGB: (41, 41, 41)
-RGB: (40, 40, 40)
-RGB: (39, 39, 39)
-RGB: (38, 38, 38)
-RGB: (37, 37, 37)
-RGB: (36, 36, 36)
-RGB: (35, 35, 35)
-RGB: (34, 34, 34)
-RGB: (33, 33, 33)
-RGB: (32, 32, 32)
-RGB: (31, 31, 31)
-RGB: (30, 30, 30)
-RGB: (29, 29, 29)
-RGB: (28, 28, 28)
-RGB: (27, 27, 27)
-RGB: (26, 26, 26)
-RGB: (25, 25, 25)
-RGB: (24, 24, 24)
-RGB: (23, 23, 23)
-RGB: (22, 22, 22)
-RGB: (21, 21, 21)
-RGB: (20, 20, 20)
-RGB: (19, 19, 19)
-RGB: (18, 18, 18)
-RGB: (17, 17, 17)
-RGB: (16, 16, 16)
-RGB: (15, 15, 15)
-RGB: (14, 14, 14)
-RGB: (13, 13, 13)
-RGB: (12, 12, 12)
-RGB: (11, 11, 11)
-RGB: (10, 10, 10)
-RGB: (9, 9, 9)
-RGB: (8, 8, 8)
-RGB: (7, 7, 7)
-RGB: (6, 6, 6)
-RGB: (5, 5, 5)
-RGB: (4, 4, 4)
-RGB: (3, 3, 3)
-RGB: (2, 2, 2)
-RGB: (1, 1, 1)
-RGB: (0, 0, 0)
+	 RGB: (99, 99, 99)
+	 RGB: (98, 98, 98)
+	 RGB: (97, 97, 97)
+	 RGB: (96, 96, 96)
+	 RGB: (95, 95, 95)
+	 RGB: (94, 94, 94)
+	 RGB: (93, 93, 93)
+	 RGB: (92, 92, 92)
+	 RGB: (91, 91, 91)
+	 RGB: (90, 90, 90)
+	 RGB: (89, 89, 89)
+	 RGB: (88, 88, 88)
+	 RGB: (87, 87, 87)
+	 RGB: (86, 86, 86)
+	 RGB: (85, 85, 85)
+	 RGB: (84, 84, 84)
+	 RGB: (83, 83, 83)
+	 RGB: (82, 82, 82)
+	 RGB: (81, 81, 81)
+	 RGB: (80, 80, 80)
+	 RGB: (79, 79, 79)
+	 RGB: (78, 78, 78)
+	 RGB: (77, 77, 77)
+	 RGB: (76, 76, 76)
+	 RGB: (75, 75, 75)
+	 RGB: (74, 74, 74)
+	 RGB: (73, 73, 73)
+	 RGB: (72, 72, 72)
+	 RGB: (71, 71, 71)
+	 RGB: (70, 70, 70)
+	 RGB: (69, 69, 69)
+	 RGB: (68, 68, 68)
+	 RGB: (67, 67, 67)
+	 RGB: (66, 66, 66)
+	 RGB: (65, 65, 65)
+	 RGB: (64, 64, 64)
+	 RGB: (63, 63, 63)
+	 RGB: (62, 62, 62)
+	 RGB: (61, 61, 61)
+	 RGB: (60, 60, 60)
+	 RGB: (59, 59, 59)
+	 RGB: (58, 58, 58)
+	 RGB: (57, 57, 57)
+	 RGB: (56, 56, 56)
+	 RGB: (55, 55, 55)
+	 RGB: (54, 54, 54)
+	 RGB: (53, 53, 53)
+	 RGB: (52, 52, 52)
+	 RGB: (51, 51, 51)
+	 RGB: (50, 50, 50)
+	 RGB: (49, 49, 49)
+	 RGB: (48, 48, 48)
+	 RGB: (47, 47, 47)
+	 RGB: (46, 46, 46)
+	 RGB: (45, 45, 45)
+	 RGB: (44, 44, 44)
+	 RGB: (43, 43, 43)
+	 RGB: (42, 42, 42)
+	 RGB: (41, 41, 41)
+	 RGB: (40, 40, 40)
+	 RGB: (39, 39, 39)
+	 RGB: (38, 38, 38)
+	 RGB: (37, 37, 37)
+	 RGB: (36, 36, 36)
+	 RGB: (35, 35, 35)
+	 RGB: (34, 34, 34)
+	 RGB: (33, 33, 33)
+	 RGB: (32, 32, 32)
+	 RGB: (31, 31, 31)
+	 RGB: (30, 30, 30)
+	 RGB: (29, 29, 29)
+	 RGB: (28, 28, 28)
+	 RGB: (27, 27, 27)
+	 RGB: (26, 26, 26)
+	 RGB: (25, 25, 25)
+	 RGB: (24, 24, 24)
+	 RGB: (23, 23, 23)
+	 RGB: (22, 22, 22)
+	 RGB: (21, 21, 21)
+	 RGB: (20, 20, 20)
+	 RGB: (19, 19, 19)
+	 RGB: (18, 18, 18)
+	 RGB: (17, 17, 17)
+	 RGB: (16, 16, 16)
+	 RGB: (15, 15, 15)
+	 RGB: (14, 14, 14)
+	 RGB: (13, 13, 13)
+	 RGB: (12, 12, 12)
+	 RGB: (11, 11, 11)
+	 RGB: (10, 10, 10)
+	 RGB: (9, 9, 9)
+	 RGB: (8, 8, 8)
+	 RGB: (7, 7, 7)
+	 RGB: (6, 6, 6)
+	 RGB: (5, 5, 5)
+	 RGB: (4, 4, 4)
+	 RGB: (3, 3, 3)
+	 RGB: (2, 2, 2)
+	 RGB: (1, 1, 1)
+	 RGB: (0, 0, 0)
 --]]
 
 -- BMP header for a user-defined pixel image (24-bit RGB)
@@ -249,113 +251,113 @@ end
 -- 	 return pixelArray
 -- end
 
- -- Function to read an ASCII text file and introduce it into a Lua table
+-- Function to read an ASCII text file and introduce it into a Lua table
 function muro.readAsciiFileIntoTable(filename)
-    local file = io.open(filename, "r") -- Open the file in read mode
+	 local file = io.open(filename, "r") -- Open the file in read mode
 
-    if not file then
-        return nil -- File couldn't be opened
-    end
+	 if not file then
+			return nil -- File couldn't be opened
+	 end
 
-    local contentTable = {} -- Table to store file content
+	 local contentTable = {} -- Table to store file content
 
-    -- Read each line and introduce it into the table
-    for line in file:lines() do
-        table.insert(contentTable, line)
-    end
+	 -- Read each line and introduce it into the table
+	 for line in file:lines() do
+			table.insert(contentTable, line)
+	 end
 
-    file:close() -- Close the file
-    return contentTable
+	 file:close() -- Close the file
+	 return contentTable
 end
 
 -- Function to horizontally mirror the patternTable
 local function mirrorPatternHorizontally(patternTable)
-    local mirroredTable = {}
+	 local mirroredTable = {}
 
-    for i = 1, #patternTable do
-        local row = patternTable[i]
-        local newRow = {}
+	 for i = 1, #patternTable do
+			local row = patternTable[i]
+			local newRow = {}
 
 
-        for j = #row, 1, -1 do
-            table.insert(newRow, row[j])
-        end
+			for j = #row, 1, -1 do
+				 table.insert(newRow, row[j])
+			end
 
-        table.insert(mirroredTable, newRow)
-    end
+			table.insert(mirroredTable, newRow)
+	 end
 
-    return mirroredTable
+	 return mirroredTable
 end
 
 -- Function to vertically mirror the patternTable
 local function mirrorPatternVertically(patternTable)
-    local mirroredTable = {}
+	 local mirroredTable = {}
 
-    for i = #patternTable, 1, -1 do
-        table.insert(mirroredTable, patternTable[i])
-    end
+	 for i = #patternTable, 1, -1 do
+			table.insert(mirroredTable, patternTable[i])
+	 end
 
-    return mirroredTable
+	 return mirroredTable
 end
 
 -- Function to scale the character table by a factor
 local function scaleCharacterTable(characterTable, factor)
-    local scaledTable = {}
+	 local scaledTable = {}
 
-    for i = 1, #characterTable do
-        local newRow = ""
-        local row = characterTable[i]
-        for j = 1, #row do
-            local character = row:sub(j, j)
-            newRow = newRow .. string.rep(character, factor)
-        end
-        for _ = 1, factor do
-            table.insert(scaledTable, newRow)
-        end
-    end
+	 for i = 1, #characterTable do
+			local newRow = ""
+			local row = characterTable[i]
+			for j = 1, #row do
+				 local character = row:sub(j, j)
+				 newRow = newRow .. string.rep(character, factor)
+			end
+			for _ = 1, factor do
+				 table.insert(scaledTable, newRow)
+			end
+	 end
 
-    return scaledTable
+	 return scaledTable
 end
 
 -- Create a user-defined array of pixel colors based on a repeating pattern table
 local function createTablePatternedPixelArray(width, height, pattern)
-    local pixelArray = {}
-    local patternWidth = #pattern[1] -- Width of the pattern
+	 local pixelArray = {}
+	 local patternWidth = #pattern[1] -- Width of the pattern
 
-    -- Generate and populate the pixelArray with the repeating pattern defined in the table
-    for row = 1, height do
-        local rowIndex = ((row - 1) % #pattern) + 1
-        for col = 1, width do
-            local colIndex = ((col - 1) % patternWidth) + 1
-            local color = pattern[rowIndex][colIndex]
-            pixelArray[(row - 1) * width + col] = color
-        end
-    end
+	 -- Generate and populate the pixelArray with the repeating pattern defined in the table
+	 for row = 1, height do
+			local rowIndex = ((row - 1) % #pattern) + 1
+			for col = 1, width do
+				 local colIndex = ((col - 1) % patternWidth) + 1
+				 local color = pattern[rowIndex][colIndex]
+				 pixelArray[(row - 1) * width + col] = color
+			end
+	 end
 
-    return pixelArray
+	 return pixelArray
 end
 
 -- Function to transform a table of characters into a valid patternTable using the color lookup table
 local function createPatternTableFromCharacters(characterTable, colorLookup)
-    local patternTable = {}
-    local height = #characterTable
-    local width = #characterTable[1]
+	 local patternTable = {}
+	 local height = #characterTable
+	 local width = #characterTable[1]
 
-    for i = 1, height do
-        patternTable[i] = {}
-        for j = 1, width do
-            local character = characterTable[i]:sub(j, j) -- Extract individual character
-            -- Retrieve color from the color lookup table
-            local color = colorLookup[character] or string.char(255, 255, 255) -- Default color (e.g., Red)
-            patternTable[i][j] = color
-        end
-    end
+	 for i = 1, height do
+			patternTable[i] = {}
+			for j = 1, width do
+				 local character = characterTable[i]:sub(j, j) -- Extract individual character
+				 -- Retrieve color from the color lookup table
+				 local color = colorLookup[character] or string.char(255, 255, 255) -- Default color (e.g., Red)
+				 patternTable[i][j] = color
+			end
+	 end
 
-    return patternTable
+	 return patternTable
 end
 
 -- Save a user-defined BMP image
-function muro.saveBMP(filename, width, height, scaling, horizontal, vertical, characterTable, colorLookup)
+function muro.saveBMP(filename, width, height, scaling, horizontal, vertical, characterTable, colorLookup, compress)
 
 	 -- Scale the character table
 	 local scalingFactor = scaling
@@ -374,7 +376,7 @@ function muro.saveBMP(filename, width, height, scaling, horizontal, vertical, ch
 
 	 -- Mirror the pattern vertically
 	 if vertical then
-	 generatedPattern = mirrorPatternVertically(generatedPattern)
+			generatedPattern = mirrorPatternVertically(generatedPattern)
 	 end
 
 	 -- Create the BMP image data
@@ -386,9 +388,21 @@ function muro.saveBMP(filename, width, height, scaling, horizontal, vertical, ch
 
 	 -- Write the BMP header and image data to the output file
 	 local outputFile = assert(io.open(filename, "wb"))
-	 outputFile:write(bmpHeader)
-	 outputFile:write(bmpImage)
+
+	 -- apply compression if needed
+	 if compress then
+			-- Compress the data using LibDeflate
+			print("compressing data...")
+			local bmpImage = bmpHeader .. bmpImage
+			local compressedData = libdeflate:CompressDeflate(bmpImage)
+			outputFile:write(compressedData)
+	 else
+			outputFile:write(bmpHeader)
+			outputFile:write(bmpImage)
+	 end
+
 	 outputFile:close()
+
 
 	 print("BMP file saved as", filename)
 end
--- main.lua
+++ main.lua
@@ -13,6 +13,7 @@ parser:flag("-v --version", version)
 parser:flag("--mirrorh",  "mirror image horizontally")
 parser:flag("--mirrorv", "mirror image vertically")
 parser:flag("--duplicatelines", "duplicates lines of the image")
+parser:flag("-c --compress", "compress the image with libdeflate")
 parser:option("-w --width", "width of the image ", 3840)
 parser:option("-e --height", "height of the image ", 2160)
 parser:option("-s --scale", "scale of the charactertable", 1)
@@ -66,4 +67,4 @@ end
 local outputBMPFilename = args.filename
 
 -- Save the BMP image
-m.saveBMP(outputBMPFilename, userWidth, userHeight, scaling, args.mirrorh, args.mirrorv, characterTable, colorLookup)
+m.saveBMP(outputBMPFilename, userWidth, userHeight, scaling, args.mirrorh, args.mirrorv, characterTable, colorLookup, args.compress)
